92a03b64585d11cab149b503c6d80b43f1abfa83
zhengfutian <futian.zheng@upnock.com>
2025-06-17 17:25:52 +0900

    郑富田-0613

--- src/components/OrderFilter.tsx
+++ src/components/OrderFilter.tsx
@@ -3,23 +3,48 @@ import { HStack, Input, Button } from "@chakra-ui/react";
 import { useState } from "react";
 
 interface Props {
-	onFilter: (keyword: string) => void;
-	zhangqinghua: string;
+  onFilter: (keyword: string, orderStatus: number) => void;
 }
 
 export function OrderFilter({ onFilter }: Props) {
-	const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
+  const [orderStatus, setOrderStatus] = useState<number>(-1);
 
-	return (
-		<HStack mb={4}>
-			<Input
-				placeholder="请输入产品名称"
-				value={input}
-				onChange={(e) => setInput(e.target.value)}
-			/>
-			<Button colorScheme="blue" onClick={() => onFilter(input)}>
-				筛选
-			</Button>
-		</HStack>
-	);
+  const reset = () => {
+    setInput("");
+    setOrderStatus(-1);
+  };
+
+  return (
+    <HStack mb={4}>
+      <Input
+        placeholder="请输入产品名称"
+        value={input}
+        onChange={(e) => setInput(e.target.value.trim())}
+        name="input_box1"
+        id="input_box1"
+        className="input_box1"
+      />
+      <select
+        value={orderStatus}
+        onChange={(e) => setOrderStatus(Number(e.target.value))}
+      >
+        <option value="-1">全部</option>
+        <option value="0">已完成</option>
+        <option value="1">已发货</option>
+        <option value="2">待发货</option>
+        <option value="3">待付款</option>
+        <option value="4">已取消</option>
+      </select>
+      <Button onClick={() => onFilter(input, orderStatus)}>筛选</Button>
+      <Button
+        onClick={() => {
+          reset();
+          onFilter("", -2); // 让OrderList变为空
+        }}
+      >
+        重置
+      </Button>
+    </HStack>
+  );
 }
--- src/mock/orders.ts
+++ src/mock/orders.ts
@@ -1,7 +1,8 @@
 // src/mock/orders.ts
 export const mockOrders = [
-	{ id: 1, productName: "iPhone 15", quantity: 2, orderStatus: 0 },
-	{ id: 2, productName: "MacBook Pro", quantity: 1, orderStatus: 1 },
-	{ id: 3, productName: "iPad Air", quantity: 3 , orderStatus: 2 },
-	{ id: 4, productName: "AirPods Pro", quantity: 0 },
+  { id: 1, productName: "iPhone 15", quantity: 2, orderStatus: 0 },
+  { id: 2, productName: "MacBook Pro", quantity: 1, orderStatus: 1 },
+  { id: 3, productName: "iPad Air", quantity: 3, orderStatus: 2 },
+  { id: 4, productName: "iPhone 16", quantity: 0, orderStatus: 3 },
+  { id: 5, productName: "iPhone 15 Pro", quantity: 0, orderStatus: 4 },
 ];
